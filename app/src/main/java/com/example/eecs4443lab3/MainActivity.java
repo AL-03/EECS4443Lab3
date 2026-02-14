f84d183107775d82668dd572e3730d914d62ce94
alexalupo <alexa.lupo5@gmail.com>
2026-02-14 11:46:31 -0500

    Created TaskValidator and check for valid input

--- app/src/main/java/com/example/eecs4443lab3/MainActivity.java
+++ app/src/main/java/com/example/eecs4443lab3/MainActivity.java
@@ -1,5 +1,15 @@
 package com.example.eecs4443lab3;
 
+import android.content.SharedPreferences;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,7 +18,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+    private EditText editTitle, editDeadline, editNotes;
+    private Button createTaskButton;
+    private long chosenDeadlineMillis=-1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editTitle=findViewById(R.id.editTitle);
+        editDeadline=findViewById(R.id.editDeadline);
+        editNotes=findViewById(R.id.editNotes);
+        createTaskButton=findViewById(R.id.createTaskButton);
+
+        calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
+            Calendar calendar=Calendar.getInstance();
+            calendar.set(year,month,dayOfMonth);
+            chosenDeadlineMillis=calendar.getTimeInMillis();
+        });
+
+        //create task button listner sends to submission handler to ensure proper validation
+        createTaskButton.setOnClickListener(v -> submissionHandler());
+
+
+    }
+    private void submissionHandler(){
+        String title=editTitle.getText().toString();
+        //if title and deadline are not valid, do not submit the task
+        if (!TaskValidator.validateTask(this,title,chosenDeadlineMillis)){
+            return;
+        }
     }
 }
+
+
+
